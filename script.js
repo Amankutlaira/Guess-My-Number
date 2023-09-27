7d84bb1092ca860ab34b39a8651be3af7d64d7ab
Aman Kutlaria <rajaman.rajput09@gmail.com>
2023-09-27 10:47:26 +0530

    Guess-my-number

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Numer! 🎊';
+
+document.querySelector('.number').textContent = 5;
+document.querySelector('.score').textContent = 12;
+
+console.log(document.querySelector('.guess').value);

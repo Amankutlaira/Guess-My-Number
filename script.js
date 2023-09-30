97087235ebc2d0f0a856b1bdef3b765d3a8883c3
Aman Kutlaria <rajaman.rajput09@gmail.com>
2023-09-30 23:41:22 +0530

    modified

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Numer! 🎊';
 
@@ -7,3 +7,63 @@ document.querySelector('.number').textContent = 5;
 document.querySelector('.score').textContent = 12;
 
 console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  //No input enter
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+
+  //when you guess correct
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 💥';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '40rem';
+  }
+
+  //when number is high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High! 🙅‍♂️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🥲 You Lost The Game!!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //when your number is low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low! ⏪';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🥲 You Lost The Game!!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log('Hello World');
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
